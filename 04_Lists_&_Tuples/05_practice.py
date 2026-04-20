4a53c4baa1ab61aadc80242e9a9c69d73c7c1fa1
Mayur-Ichake <mayurichake4@gmail.com>
2026-04-20 22:51:02 +0530

    done

--- 04_Lists_&_Tuples/05_practice.py
+++ 04_Lists_&_Tuples/05_practice.py
@@ -1,4 +1,4 @@
-a = [1,2,3,4,5,6]
+"""a = [1,2,3,4,5,6]
 
 b = len(a)-1
 
@@ -6,4 +6,9 @@ for i in range(0,len(a)+1):
     a[i] == a[b-len(a)]
     b -= 1
 
-print(a)
+print(a)"""
+
+a = 10
+print(type(a))
+
+print(isinstance({},dict))
