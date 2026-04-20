1d91e40612c227e77f262834908fee9f283ced10
Mayur-Ichake <mayurichake4@gmail.com>
2026-04-20 22:58:24 +0530

    done

--- 04_Lists_&_Tuples/05_practice.py
+++ 04_Lists_&_Tuples/05_practice.py
@@ -1,14 +1,2 @@
-"""a = [1,2,3,4,5,6]
-
-b = len(a)-1
-
-for i in range(0,len(a)+1):
-    a[i] == a[b-len(a)]
-    b -= 1
-
-print(a)"""
-
 import keyword
-keyword.kwlist
-a = 10
-print(id(a))
+print(keyword.kwlist)
